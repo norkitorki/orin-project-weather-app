86427971193ad6487434a10e8dcc585c49b6425b
norkitorki <norki@tuta.io>
2024-07-17 12:48:17 +0200

    Create queries module responsible for making api requests

---
+++ src/components/queries.js
@@ -0,0 +1,26 @@
+const API_KEY = process.env.WEATHER_API_KEY;
+console.log(API_KEY);
+
+export function getLocationData(query) {
+  const url = `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`;
+  return fetchData(url);
+}
+
+export function getWeatherData(query, forecastDays = 3) {
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${query}&days=${forecastDays}&aqi=no&alerts=no`;
+  return fetchData(url);
+}
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+
+    if (!response.ok) {
+      throw new Error('A network error has occured. Please check your internet connection and try again.');
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
