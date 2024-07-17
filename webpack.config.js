d95912f8eda3cbae68035b3b001ba0a24ff27f73
norkitorki <norki@tuta.io>
2024-07-17 11:19:27 +0200

    Install dotenv-webpack as dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,4 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  plugins: [
+    new Dotenv(),
+  ],
 };
