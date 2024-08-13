74772d2ff32c667e9663c07fa8b77dfa7fbf03c2
norkitorki <norki@tuta.io>
2024-08-13 11:22:39 +0200

    Complete weather_display component functionality

--- src/components/weather_display.js
+++ src/components/weather_display.js
@@ -1,33 +1,38 @@
 const error = document.querySelector('.error');
-const locationName = document.querySelector('.location-name');
-const conditionIcon = document.querySelector('.condition-icon');
-const temperature = document.querySelector('.current-temperature');
-const condition = document.querySelector('.condition');
-const typeBtn = document.getElementById('convert-unit');
-const refreshBtn = document.querySelector('.refresh-weather');
 
-localStorage.setItem('unit', localStorage.getItem('unit') || 'c');
+localStorage.setItem('unit', localStorage.getItem('unit') || 'm');
 
-export function displayWeather(refreshCallback) {
-  const weather = JSON.parse(String(localStorage.getItem('weather_data')));
+export function displayWeather(refreshCallback, day = []) {
+  const data = JSON.parse(String(localStorage.getItem('weather_data')));
 
-  if (weather) {
-    const forecasts = weather.forecast.forecastday;
-    const localHour = new Date(weather.current.last_updated).getHours();
+  if (data) {
+    const refreshBtn = document.getElementById('refresh-weather');
+    const convertBtn = document.getElementById('convert-unit');
 
-    updateCurrentWeather(weather, forecasts[0], localHour);
-    updateWeatherForecast(forecasts, localHour);
+    refreshBtn.parentElement.classList.remove('hidden');
+
+    if (refreshCallback) {
+      refreshBtn.addEventListener('click', () => {
+        refreshCallback.call(this, localStorage.getItem('location_url'));
+      });
+    }
+
+    convertBtn.textContent =
+      localStorage.getItem('unit') === 'm' ? 'Metric' : 'Imperial';
+    convertBtn.addEventListener('click', convertUnits);
+
+    const forecasts = data.forecast.forecastday;
+    const localHour = new Date(data.current.last_updated).getHours();
+
+    if (day.length > 0) {
+      const lastUpdate = data.current.last_updated;
+      data.current = data.forecast.forecastday[day[0]].hour[day[1]];
+      data.current.last_updated = lastUpdate;
+    }
+
+    updateCurrentWeather(data.current, data.location);
+    updateWeatherForecast(forecasts, localHour, data.location);
     document.querySelector('.weather-data').classList.remove('hidden');
-
-    typeBtn.classList.remove('hidden');
-    typeBtn.textContent =
-      localStorage.getItem('unit') === 'c' ? 'Celsius' : 'Fahrenheit';
-    typeBtn.addEventListener('click', convertTemperatures);
-
-    refreshBtn.addEventListener(
-      'click',
-      refreshCallback.bind(this, localStorage.getItem('location_url'))
-    );
 
     return true;
   } else {
@@ -35,29 +40,76 @@ export function displayWeather(refreshCallback) {
   }
 }
 
-function updateCurrentWeather(data, forecast, localHour) {
-  locationName.textContent = `${data.location.name} (${data.location.region}), ${data.location.country}`;
-  conditionIcon.src = data.current.condition.icon.replace(/^\/\//, 'https://');
-  setTemperatures(forecast.hour[localHour - 1], temperature);
-  condition.textContent = data.current.condition.text;
+function updateCurrentWeather(weatherData, locationData) {
+  console.log(weatherData);
+
+  const container = document.querySelector('.weather-data > .current');
+  container.innerHTML = '';
+
+  const template = document
+    .getElementById('weather-template')
+    .content.cloneNode(true);
+
+  const locationName = template.querySelector('.location-name');
+  const locationTime = template.querySelector('.location-time');
+  const conditionIcon = template.querySelector('.condition-icon');
+  const condition = template.querySelector('.condition');
+  const lastUpdate = template.querySelector('.last-update');
+  const temperature = template.querySelector('.temperature');
+  const humidity = template.querySelector('.humidity');
+  const airPressure = template.querySelector('.air-pressure');
+  const precipitation = template.querySelector('.precipitation');
+  const wind = template.querySelector('.wind');
+  const windDirection = template.querySelector('.wind-direction');
+  const visibility = template.querySelector('.visibility');
+
+  lastUpdate.textContent = JSON.parse(
+    localStorage.getItem('weather_data')
+  ).current.last_updated.split(' ')[1];
+
+  locationName.textContent = `${locationData.name} (${locationData.region}), ${locationData.country}`;
+  locationTime.textContent = (
+    weatherData.time || weatherData.last_updated
+  ).match(/\d{2}:\d{2}$/)[0];
+
+  conditionIcon.src = weatherData.condition.icon.replace(/^\/\//, 'https://');
+  condition.textContent = weatherData.condition.text;
+
+  humidity.textContent = `${weatherData.humidity}%`;
+
+  setTemperatures(weatherData, temperature);
+  setAirPressure(weatherData, airPressure);
+  setPrecipitation(weatherData, precipitation);
+  setWind(weatherData, wind, windDirection);
+  setVisibility(weatherData, visibility);
+
+  container.prepend(template);
 }
 
-function updateWeatherForecast(data, localHour) {
-  const container = document.querySelector('.weather-forecasts');
+function updateWeatherForecast(data, localHour, locationData) {
+  const container = document.querySelector('.forecasts');
   container.innerHTML = '';
 
   let template, image, temperature, time;
-  const days = ['Today', 'Tomorrow', 'In 2 days'];
+  const dayDescriptor = ['Today', 'Tomorrow', 'In 2 days'];
 
   data.forEach((day, i) => {
     template = document
       .getElementById('forecast-template')
       .content.cloneNode(true);
 
-    template.querySelector('h3').textContent = `${days[i]} (${day.date})`;
+    template.querySelector(
+      'h3'
+    ).textContent = `${dayDescriptor[i]} (${day.date})`;
 
     day.hour.forEach((hour, y) => {
       const element = template.querySelector(`.hour-${y}`);
+
+      element.addEventListener('click', () => {
+        updateCurrentWeather(hour, locationData, null);
+        const locationInfo = document.querySelector('.weather-data');
+        locationInfo.scrollIntoView({ behavior: 'smooth' });
+      });
 
       if (i === 0) {
         if (y < localHour) {
@@ -71,7 +123,7 @@ function updateWeatherForecast(data, localHour) {
       time.textContent = y;
 
       image = document.createElement('img');
-      image.src = hour.condition.icon.replace(/^\/\//, 'https://');
+      image.src = `https:${hour.condition.icon}`;
 
       temperature = document.createElement('span');
       temperature.classList.add('temp');
@@ -94,47 +146,86 @@ export function selectLocation(locations, callback) {
     button.textContent = `${location.name}(${location.region}), ${location.country}`;
     button.addEventListener('click', () => {
       container.innerHTML = '';
-      callback(location.url);
+      if (callback) {
+        callback.call(this, location.url);
+      }
     });
 
     container.appendChild(button);
   });
-
-  document.body.insertBefore(container, form);
 }
 
-function colorTemps(element) {
-  element.classList.value = 'temp';
-  const currentTemp = Number(element.dataset['c']);
+function colorTemps() {
+  this.classList.value = 'temp';
+  const currentTemp = Number(this.dataset['m'].match(/^\d+/));
 
   if (currentTemp > 30) {
-    element.classList.add('very-hot');
+    this.classList.add('very-hot');
   } else if (currentTemp > 20) {
-    element.classList.add('hot');
+    this.classList.add('hot');
   } else if (currentTemp < 10) {
-    element.classList.add('cold');
+    this.classList.add('cold');
   }
 }
 
-function convertTemperatures(e) {
-  const value = e.target.textContent === 'Celsius' ? 'Fahrenheit' : 'Celsius';
-  const unit = value[0].toLowerCase();
-  localStorage.setItem('unit', unit);
-
-  document.querySelectorAll('.temp').forEach((el) => {
-    colorTemps(el);
-    el.textContent = `${el.dataset[`${unit}`]} ${unit}°`;
-  });
-
-  e.target.textContent = value;
+function setTemperatures(data, element) {
+  setUnits(
+    `${Math.round(data.temp_c)} c°`,
+    `${Math.round(data.temp_f)} f°`,
+    element,
+    [colorTemps.bind(element)]
+  );
 }
 
-function setTemperatures(data, element) {
-  element.dataset['c'] = Math.round(data.temp_c);
-  element.dataset['f'] = Math.round(data.temp_f);
-  const unit = localStorage.getItem('unit');
-  colorTemps(element);
-  element.textContent = `${element.dataset[unit]} ${unit}°`;
+function setAirPressure(data, element) {
+  setUnits(`${data.pressure_mb} mb`, `${data.pressure_in} in`, element);
+}
+
+function setPrecipitation(data, element) {
+  setUnits(`${data.precip_mm} mm`, `${data.precip_in} in`, element);
+}
+
+function setWind(data, element, windDirectionArrow) {
+  setUnits(
+    `${data.wind_dir}(${data.wind_degree}deg) at ${Math.round(
+      data.wind_kph
+    )} kph`,
+    `${data.wind_dir}(${data.wind_degree}deg) at ${Math.round(
+      data.wind_mph
+    )} mph`,
+    element
+  );
+
+  windDirectionArrow.setAttribute(
+    'style',
+    `transform:rotate(${data.wind_degree}deg)`
+  );
+}
+
+function setVisibility(data, element) {
+  setUnits(`${data.vis_km} km`, `${data.vis_miles} miles`, element);
+}
+
+function setUnits(value_m, value_i, element, callbacks = []) {
+  element.dataset['m'] = value_m;
+  element.dataset['i'] = value_i;
+
+  const unit = localStorage.getItem('unit') || 'm';
+  element.textContent = element.dataset[unit];
+
+  callbacks.forEach((cb) => cb.call());
+}
+
+function convertUnits(e) {
+  const currentUnit = localStorage.getItem('unit');
+  const newUnit = currentUnit === 'm' ? 'i' : 'm';
+
+  document.querySelectorAll(`[data-${newUnit}]`).forEach((el) => {
+    el.textContent = el.dataset[newUnit];
+  });
+
+  e.target.textContent = newUnit === 'm' ? 'Metric' : 'Imperial';
+  localStorage.setItem('unit', newUnit);
 }
 
 export function displayError(message) {
